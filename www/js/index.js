5afdd98f2abcf593247bdc3ef22a68d462a025c7
Roberto Rivera Reyes <roberto.rivera16@upr.edu>
2017-03-15 15:21:56 -0400

    Form addition

--- www/js/index.js
+++ www/js/index.js
@@ -56,15 +56,17 @@ function getPosition() {
 
     function onSuccess(position) {
 
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        
+        
     };
+    
+    
+    
+    
+    
+    
 
     function onError(error) {
         alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
